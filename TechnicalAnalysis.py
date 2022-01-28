151de30dc241eff8679d35bc69f46be21aa4e5b1
a1234jehad <jehad56@hotmail.com>
2022-01-28 23:52:25 +0300

    Update TechnicalAnalysis.py

--- TechnicalAnalysis.py
+++ TechnicalAnalysis.py
@@ -174,13 +174,13 @@ class TechnicalAnalysis:
 
         fig.show()
 
-    def plot_death_Golden_crosses_US(self):
+    def plot_death_Golden_crosses(self):
         '''
           When a Death Cross occurs, that is a sign that a major sell off will occur. A Death Cross is said to occur typically
           when the 50 day moving average falls below a 200 day. A Golden Cross accures when the short term average crosses
           the long term again moving higher.
           '''
-        gspc_df = yf.download(tickers='^gspc', period='max', interval='1d')
+        gspc_df = self.stock_df
         gspc_ma50 = gspc_df['Adj Close'].rolling(window=50).mean()
         gspc_ma200 = gspc_df['Adj Close'].rolling(window=200).mean()
         ma50 = go.Scatter(x=gspc_ma50.index, y=gspc_ma50,
@@ -396,11 +396,131 @@ class TechnicalAnalysis:
 
         fig.show()
 
+    def plot_Ichimoku(self):
+        """
+        The Ichimoku (One Look) is considered an all in one indicator. It provides information on momentum, support and
+        resistance. It is made up of 5 lines. If you are a short term trader you create 1 minute or 6 hour. Long term
+        traders focus on day or weekly data.
 
-TA = TechnicalAnalysis("AMD", "2mo", "1h")
+        Conversion Line (Tenkan-sen) : Represents support, resistance and reversals. Used to measure short term trends.
+        Baseline (Kijun-sen) : Represents support, resistance and confirms trend changes. Allows you to evaluate the
+        strength of medium term trends. Called the baseline because it lags the price.
+        Leading Span A (Senkou A) : Used to identify future areas of support and resistance
+        Leading Span B (Senkou B) : Other line used to identify suture support and resistance
+        Lagging Span (Chikou) : Shows possible support and resistance. It is used to confirm signals obtained from other
+        lines.
+        Cloud (Kumo) : Space between Span A and B. Represents the divergence in price evolution.
+
+         What the Lines Mean??
+
+         Lagging Span : When above the price it is bullish and when below bearish. It is used
+         with other indicators because it is mainly a filter.
+
+         Baseline : When below price this is considered support.
+         When above price this is considered resistance. We are in an uptrend when the slope increases and vice
+         versa. The slope of the curve tells us the strength of the trend.
+
+         Conversion : We focus on its position
+         versus the Baseline. When the Conversion crosses above the Baseline we are in an upward trend and vice
+         versa. This is considered a strong indicator when above the Cloud and weak when below.
+
+         Cloud : The thicker
+         the Cloud, the stronger the trend and vice versa. When the Spans cross many times we are in a range. When
+         they cross this is a sign of a reversal of trend.
+        """
+        def get_fill_color(label):
+            if label >= 1:
+                return 'rgba(0,250,0,0.4)'
+            else:
+                return 'rgba(250,0,0,0.4)'
+        df = self.stock_df
+
+        # Conversion
+        hi_val = df['High'].rolling(window=9).max()
+        low_val = df['Low'].rolling(window=9).min()
+        df['Conversion'] = (hi_val + low_val) / 2
+
+        # Baseline
+        hi_val2 = df['High'].rolling(window=26).max()
+        low_val2 = df['Low'].rolling(window=26).min()
+        df['Baseline'] = (hi_val2 + low_val2) / 2
+
+        # Spans
+        df['SpanA'] = ((df['Conversion'] + df['Baseline']) / 2).shift(26)
+        hi_val3 = df['High'].rolling(window=52).max()
+        low_val3 = df['Low'].rolling(window=52).min()
+        df['SpanB'] = ((hi_val3 + low_val3) / 2).shift(26)
+        df['Lagging'] = df['Close'].shift(-26)
+
+        candle = go.Candlestick(x=df.index, open=df['Open'],
+                                high=df['High'], low=df['Low'],
+                                close=df['Close'], name='Candlestick')
+
+        df1 = df.copy()
+        fig = go.Figure()
+        df['label'] = np.where(df['SpanA'] > df['SpanB'], 1, 0)
+
+        df['group'] = df['label'].ne(df['label'].shift()).cumsum()
+
+        df = df.groupby('group')
+
+        dfs = []
+        for name, data in df:
+            dfs.append(data)
+
+        for df in dfs:
+            fig.add_traces(go.Scatter(x=df.index, y=df.SpanA,
+                                      line=dict(color='rgba(0,0,0,0)')))
+
+            fig.add_traces(go.Scatter(x=df.index, y=df.SpanB,
+                                      line=dict(color='rgba(0,0,0,0)'),
+                                      fill='tonexty',
+                                      fillcolor=get_fill_color(df['label'].iloc[0])))
+
+        baseline = go.Scatter(x=df1.index, y=df1['Baseline'],
+                              line=dict(color='pink', width=2), name="Baseline")
+
+        conversion = go.Scatter(x=df1.index, y=df1['Conversion'],
+                                line=dict(color='black', width=1), name="Conversion")
+
+        lagging = go.Scatter(x=df1.index, y=df1['Lagging'],
+                             line=dict(color='purple', width=2, dash='dot'), name="Lagging")
+
+        span_a = go.Scatter(x=df1.index, y=df1['SpanA'],
+                            line=dict(color='green', width=2, dash='dot'), name="Span A")
+
+        span_b = go.Scatter(x=df1.index, y=df1['SpanB'],
+                            line=dict(color='red', width=1, dash='dot'), name="Span B")
+
+        # Add plots to the figure
+        fig.add_trace(candle)
+        fig.add_trace(baseline)
+        fig.add_trace(conversion)
+        fig.add_trace(lagging)
+        fig.add_trace(span_a)
+        fig.add_trace(span_b)
+
+        fig.update_layout(height=900, width=1000,
+                          showlegend=True)
+        if (self.ticker[-2:] == "SR"):
+            if self.intv == '1d':
+                fig.update_xaxes(rangebreaks=[dict(bounds=["fri", "sun"])])
+            else:
+                fig.update_xaxes(rangebreaks=[dict(bounds=["fri", "sun"]), dict(bounds=[15, 10], pattern="hour")])
+
+        else:
+            if self.intv == '1d':
+                fig.update_xaxes(rangebreaks=[dict(bounds=["sat", "mon"])])
+            else:
+                fig.update_xaxes(rangebreaks=[dict(bounds=["sat", "mon"]), dict(bounds=[16, 9.5], pattern="hour")])
+
+        fig.show()
+
+TA = TechnicalAnalysis("AMD", "1y", "1d")
 # print(TA.stock_df)
 # TA.plot_macd()
 # print(TA.stock_df)
 # TA.plot_death_Golden_crosses_US()
 #TA.plot_RSI()
 TA.plot_Bollinger_bands()
+TA.plot_Ichimoku()
